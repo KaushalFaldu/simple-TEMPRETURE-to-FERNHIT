5b974f2980aafd2abab46bd82cebb2e4147fd008
Kaushal Faldu <85346182+kaushal1610@users.noreply.github.com>
2022-04-10 09:32:56 +0530

    Add files via upload

---
+++ Program 9.py
@@ -0,0 +1,6 @@
+#30 Shruti nathavani
+sn_tmp_c = float(input("ENTER VALUE OF TEMPRETURE IN CELCIUS THAT YOU WANT TO CONVERT IN FERNHIT : ")) #input the value of TEMPRETURE
+sn_tmp_f = (((9/5)*sn_tmp_c)+32)
+print(sn_tmp_f)
+sn_tmp_f = ((sn_tmp_c-32)*(5/9))
+print(sn_tmp_c)
